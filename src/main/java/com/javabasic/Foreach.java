f7ec28cc481d2c48a94d301744e81c11ad52077b
manojkumarkasiviswanathan <manojkumar.kasiviswanathan@gmail.com>
2022-08-26 14:08:52 +0100

    added foreach loop

---
+++ src/main/java/com/javabasic/Foreach.java
@@ -0,0 +1,13 @@
+package com.javabasic;
+
+public class Foreach {
+
+    public static void main(String[] args) {
+
+        int values[] = {1, 2, 3, 4, 5};
+        for (int value : values) {
+            System.out.println(value);
+        }
+    }
+
+}
