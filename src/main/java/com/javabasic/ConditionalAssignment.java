fdc3ad761d06289a4dfc2dd292def1f789126e63
Manojkumar kasiviswanathan <112106236+manojkumar-kasiviswanathan@users.noreply.github.com>
2025-04-30 17:25:45 +0100

    Update ConditionalAssignment.java

--- src/main/java/com/javabasic/ConditionalAssignment.java
+++ src/main/java/com/javabasic/ConditionalAssignment.java
@@ -1,14 +1,1 @@
-        RestAssuredConfig config =
-                RestAssured.config()
-                        .httpClient(
-                                HttpClientConfig.httpClientConfig()
-                                        .httpClientFactory(
-                                                () ->
-                                                        HttpClientBuilder.create()
-                                                                .setDefaultRequestConfig(
-                                                                        RequestConfig.custom()
-                                                                                .setConnectTimeout(20000)
-                                                                                .setConnectionRequestTimeout(20000)
-                                                                                .setSocketTimeout(20000)
-                                                                                .build())
-                                                                .build()));
+
